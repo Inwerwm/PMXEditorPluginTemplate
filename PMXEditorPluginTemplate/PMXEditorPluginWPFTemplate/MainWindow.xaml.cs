87ccd100880962f51b515f54d1a15a8f133194c8
Inwerwm <saclainvervm@gmail.com>
2021-11-04 20:03:59 +0900

    WPF用テンプレートを追加

---
+++ PMXEditorPluginTemplate/PMXEditorPluginWPFTemplate/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using PEPlugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PMXEditorPluginWPFTemplate
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IPERunArgs Args { get; }
+
+        public MainWindow(IPERunArgs args)
+        {
+            Args = args;
+
+            InitializeComponent();
+            Reload();
+        }
+
+        public void Reload()
+        {
+            // TODO: プラグインの再読み込み処理を記述します。
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+    }
+}
