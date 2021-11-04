2a3e7101c946560e378125f7b6945fabbbfbf95f
Inwerwm <saclainvervm@gmail.com>
2021-11-04 20:03:59 +0900

    WPF用テンプレートを追加

---
+++ PMXEditorPluginTemplate/PMXEditorPluginWPFTemplate/PluginMain.cs
@@ -0,0 +1,60 @@
+﻿using PEPlugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMXEditorPluginWPFTemplate
+{
+    public class PluginMain : IPEPlugin
+    {
+        private bool disposedValue;
+
+        public string Name => "$safeprojectname$";
+
+        public string Version => "0.0";
+
+        public string Description => "$safeprojectname$";
+
+        public IPEPluginOption Option => new PEPluginOption();
+
+        private MainWindow Window { get; set; }
+
+        public void Run(IPERunArgs args)
+        {
+            Window = Window ?? new MainWindow(args);
+            Window.Reload();
+            Window.Show();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                }
+
+                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+                // TODO: 大きなフィールドを null に設定します
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+        // ~PluginMain()
+        // {
+        //     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
